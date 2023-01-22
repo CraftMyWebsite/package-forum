4db1ec82371fe1e11c6b227ad14136fec6feaec3
Emilien52 <emiliengarandeau@yahoo.fr>
2023-01-22 14:24:25 +0100

    Ajout de en.php
    Il est possible que la traduction de la ligne 22 soit fausse (à vérifier)

---
+++ lang/en.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    "category" => [
+        "add" => [
+            "title" => "Forum | Category | Add",
+            "description" => "Add a new category to your site",
+            "card_title" => "Add a category",
+        ],
+        "list" => [
+            "title" => "Forum | Category",
+            "description" => "Manage your forum categories",
+            "card_title" => "List of all your categories",
+        ],
+        "name" => "Category name",
+        "description" => "Category Description",
+    ],
+
+    "forum" => [
+        "list" => [
+            "title" => "Forum | Forum",
+            "desc" => "erate the forums of your forum",
+            "card_title" => "List of all your forums",
+        ],
+        "name" => "Name of forum",
+        "description" => "Forum description"
+    ],
+
+    "id" => "ID",
+    "name" => "Name",
+    "description" => "Description",
+    "action" => "Action",
+    "parent" => "Parent",
+
+    "btn" => [
+        "add_category" => "Add a catégory",
+        "add_forum" => "Add a forum",
+    ],
+];
