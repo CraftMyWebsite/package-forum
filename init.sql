68ec26eb4723ca3540259b9ac511e48cc4c8f8e7
Zomblard <leo.goigoux@gmail.com>
2023-03-27 17:47:20 +0200

    [ADD] DB Timestamp

--- init.sql
+++ init.sql
@@ -2,7 +2,9 @@ CREATE TABLE IF NOT EXISTS cmw_forums_categories
 (
     forum_category_id          INT AUTO_INCREMENT PRIMARY KEY,
     forum_category_name        VARCHAR(50) NOT NULL,
-    forum_category_description TEXT        NULL
+    forum_category_description TEXT        NULL,
+    forum_category_created TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    forum_category_updated TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
@@ -14,6 +16,8 @@ CREATE TABLE IF NOT EXISTS cmw_forums
     forum_description TEXT         NULL,
     forum_subforum_id INT          NULL,
     forum_category_id INT          NULL,
+    forum_created TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    forum_updated TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT fk_forum_category_id
         FOREIGN KEY (forum_category_id) REFERENCES cmw_forums_categories (forum_category_id) ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT fk_forum_forum_id
@@ -32,6 +36,8 @@ CREATE TABLE IF NOT EXISTS cmw_forums_topics
     forum_topic_important        TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
     user_id                      INT                 NULL,
     forum_id                     INT                 NOT NULL,
+    forum_topic_created TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    forum_topic_updated TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT fk_forum_id
         FOREIGN KEY (forum_id) REFERENCES cmw_forums (forum_id) ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT fk_topics_user_id
@@ -55,6 +61,8 @@ CREATE TABLE IF NOT EXISTS cmw_forums_response
     forum_response_id      INT AUTO_INCREMENT PRIMARY KEY,
     forum_response_content TEXT NOT NULL,
     forum_topic_id         INT  NOT NULL,
+    forum_response_created TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    forum_response_updated TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     user_id                INT  NOT NULL,
     CONSTRAINT fk_response_forum_topic_id
         FOREIGN KEY (forum_topic_id) REFERENCES cmw_forums_topics (forum_topic_id) ON UPDATE CASCADE ON DELETE CASCADE,
