bdae221ccd60e3faf7a42dcd64479309cd97d72a
Teyir <60007549+Teyir@users.noreply.github.com>
2023-06-21 20:45:27 +0200

    [FIX] Fix init.sql

--- Init/init.sql
+++ Init/init.sql
@@ -9,22 +9,24 @@ CREATE TABLE IF NOT EXISTS cmw_forums_categories
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS cmw_forums_topics_views
-(
-    forum_topics_views_id       INT AUTO_INCREMENT PRIMARY KEY,
-    forum_topics_views_topic_id INT         NOT NULL,
-    forum_topics_views_ip       VARCHAR(50) NOT NULL,
-    CONSTRAINT fk_views_topic_id FOREIGN KEY (forum_topics_views_topic_id)
-        REFERENCES cmw_forums_topics (forum_topic_id) ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
-
 CREATE TABLE IF NOT EXISTS cmw_forums_settings
 (
     forum_settings_id      INT AUTO_INCREMENT PRIMARY KEY,
     forum_settings_name    VARCHAR(50)  NOT NULL,
     forum_settings_value   VARCHAR(200) NULL,
     forum_settings_updated TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS cmw_forums_prefixes
+(
+    forum_prefix_id          INT AUTO_INCREMENT PRIMARY KEY,
+    forum_prefix_name        VARCHAR(50) NOT NULL,
+    forum_prefix_color       VARCHAR(50) NULL,
+    forum_prefix_text_color  VARCHAR(50) NULL,
+    forum_prefix_description TEXT        NULL,
+    forum_prefix_created     TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    forum_prefix_updated     TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
@@ -106,57 +108,13 @@ VALUES ('IconNotRead', 'fa-solid fa-eye'),
        ('IconClosed', 'fa-solid fa-lock');
 
 
-CREATE TABLE IF NOT EXISTS cmw_forums_discord
+CREATE TABLE IF NOT EXISTS cmw_forums_topics_views
 (
-    `forum_discord_id`   INT AUTO_INCREMENT PRIMARY KEY,
-    `forum_discord_webhook` VARCHAR(255) NOT NULL,
-    `forum_discord_description` VARCHAR(50) NOT NULL,
-    `forum_discord_embed_color` VARCHAR(50) NOT NULL,
-    `forum_category_id`  INT(11) NULL,
-    `forum_prefix_id`  INT(11) NULL,
-    `forum_feedback_id`  INT(11) NULL,
-    `forum_topics_feedback_id`  INT(11) NULL,
-    `forum_response_feedback_id`  INT(11) NULL,
-    `forum_topics_views_id`  INT(11) NULL,
-    `forum_settings_id`  INT(11) NULL,
-    `forum_id`  INT(11) NULL,
-    `forum_topic_id`  INT(11) NULL,
-    `forums_topics_tags_id`  INT(11) NULL,
-    `forum_response_id`  INT(11) NULL,
-    CONSTRAINT `discord_biding_forum_category_id` FOREIGN KEY (`forum_category_id`)
-        REFERENCES `cmw_forums_categories` (`forum_category_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `discord_biding_forum_prefix_id` FOREIGN KEY (`forum_prefix_id`)
-        REFERENCES `cmw_forums_prefixes` (`forum_prefix_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `discord_biding_forum_feedback_id` FOREIGN KEY (`forum_feedback_id`)
-        REFERENCES `cmw_forums_feedback` (`forum_feedback_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `discord_biding_forum_topics_feedback_id` FOREIGN KEY (`forum_topics_feedback_id`)
-        REFERENCES `cmw_forums_topics_feedback` (`forum_topics_feedback_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `discord_biding_forum_response_feedback_id` FOREIGN KEY (`forum_response_feedback_id`)
-        REFERENCES `cmw_forums_response_feedback` (`forum_response_feedback_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `discord_biding_forum_topics_views_id` FOREIGN KEY (`forum_topics_views_id`)
-        REFERENCES `cmw_forums_topics_views` (`forum_topics_views_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `discord_biding_forum_settings_id` FOREIGN KEY (`forum_settings_id`)
-        REFERENCES `cmw_forums_settings` (`forum_settings_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `discord_biding_forum_id` FOREIGN KEY (`forum_id`)
-        REFERENCES `cmw_forums` (`forum_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `discord_biding_forum_topic_id` FOREIGN KEY (`forum_topic_id`)
-        REFERENCES `cmw_forums_topics` (`forum_topic_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `discord_biding_forums_topics_tags_id` FOREIGN KEY (`forums_topics_tags_id`)
-        REFERENCES `cmw_forums_topics_tags` (`forums_topics_tags_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT `discord_biding_forum_response_id` FOREIGN KEY (`forum_response_id`)
-        REFERENCES `cmw_forums_response` (`forum_response_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
-
-CREATE TABLE IF NOT EXISTS cmw_forums_prefixes
-(
-    forum_prefix_id          INT AUTO_INCREMENT PRIMARY KEY,
-    forum_prefix_name        VARCHAR(50) NOT NULL,
-    forum_prefix_color       VARCHAR(50) NULL,
-    forum_prefix_text_color  VARCHAR(50) NULL,
-    forum_prefix_description TEXT        NULL,
-    forum_prefix_created     TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    forum_prefix_updated     TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    forum_topics_views_id       INT AUTO_INCREMENT PRIMARY KEY,
+    forum_topics_views_topic_id INT         NOT NULL,
+    forum_topics_views_ip       VARCHAR(50) NOT NULL,
+    CONSTRAINT fk_views_topic_id FOREIGN KEY (forum_topics_views_topic_id)
+        REFERENCES cmw_forums_topics (forum_topic_id) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
@@ -244,6 +202,48 @@ CREATE TABLE IF NOT EXISTS cmw_forums_roles_permissions
         REFERENCES cmw_forums_permissions (forum_permission_id)ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT fk_forum_roles_permissions_role_id FOREIGN KEY (forum_role_id)
         REFERENCES cmw_forums_roles (forums_role_id)ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE IF NOT EXISTS cmw_forums_discord
+(
+    `forum_discord_id`   INT AUTO_INCREMENT PRIMARY KEY,
+    `forum_discord_webhook` VARCHAR(255) NOT NULL,
+    `forum_discord_description` VARCHAR(50) NOT NULL,
+    `forum_discord_embed_color` VARCHAR(50) NOT NULL,
+    `forum_category_id`  INT(11) NULL,
+    `forum_prefix_id`  INT(11) NULL,
+    `forum_feedback_id`  INT(11) NULL,
+    `forum_topics_feedback_id`  INT(11) NULL,
+    `forum_response_feedback_id`  INT(11) NULL,
+    `forum_topics_views_id`  INT(11) NULL,
+    `forum_settings_id`  INT(11) NULL,
+    `forum_id`  INT(11) NULL,
+    `forum_topic_id`  INT(11) NULL,
+    `forums_topics_tags_id`  INT(11) NULL,
+    `forum_response_id`  INT(11) NULL,
+    CONSTRAINT `discord_biding_forum_category_id` FOREIGN KEY (`forum_category_id`)
+        REFERENCES `cmw_forums_categories` (`forum_category_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `discord_biding_forum_prefix_id` FOREIGN KEY (`forum_prefix_id`)
+        REFERENCES `cmw_forums_prefixes` (`forum_prefix_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `discord_biding_forum_feedback_id` FOREIGN KEY (`forum_feedback_id`)
+        REFERENCES `cmw_forums_feedback` (`forum_feedback_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `discord_biding_forum_topics_feedback_id` FOREIGN KEY (`forum_topics_feedback_id`)
+        REFERENCES `cmw_forums_topics_feedback` (`forum_topics_feedback_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `discord_biding_forum_response_feedback_id` FOREIGN KEY (`forum_response_feedback_id`)
+        REFERENCES `cmw_forums_response_feedback` (`forum_response_feedback_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `discord_biding_forum_topics_views_id` FOREIGN KEY (`forum_topics_views_id`)
+        REFERENCES `cmw_forums_topics_views` (`forum_topics_views_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `discord_biding_forum_settings_id` FOREIGN KEY (`forum_settings_id`)
+        REFERENCES `cmw_forums_settings` (`forum_settings_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `discord_biding_forum_id` FOREIGN KEY (`forum_id`)
+        REFERENCES `cmw_forums` (`forum_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `discord_biding_forum_topic_id` FOREIGN KEY (`forum_topic_id`)
+        REFERENCES `cmw_forums_topics` (`forum_topic_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `discord_biding_forums_topics_tags_id` FOREIGN KEY (`forums_topics_tags_id`)
+        REFERENCES `cmw_forums_topics_tags` (`forums_topics_tags_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `discord_biding_forum_response_id` FOREIGN KEY (`forum_response_id`)
+        REFERENCES `cmw_forums_response` (`forum_response_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
