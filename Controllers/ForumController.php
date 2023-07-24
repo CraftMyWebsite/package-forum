62bc7e401672c487c4bd8ccdf0a035e5c16a3d71
Zomblard <leo.goigoux@gmail.com>
2023-07-24 13:59:58 +0200

    [Add] public permission check

--- Controllers/ForumController.php
+++ Controllers/ForumController.php
@@ -98,6 +98,27 @@ class ForumController extends AbstractController
     }
 
     /*--------PERM UTILS--------*/
+
+    /**
+     * @param string $permCode
+     * @return bool
+     * @desc used in public view : return true if user have permission to do something like "user_create_topic"
+     */
+    public function hasPermission(string $permCode) :bool
+    {
+        $userId = UsersModel::getCurrentUser()->getId();
+
+        if (!ForumPermissionModel::getInstance()->hasForumPermission($userId, $permCode)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param string $permCode
+     * @return void
+     * @desc used in controller : Redirect to previous route if the user don't have the permission
+     */
     public function redirectIfNotHavePermissions(string $permCode): void
     {
         if (!UsersController::isUserLogged()) {
@@ -108,15 +129,23 @@ class ForumController extends AbstractController
         $userId = UsersModel::getCurrentUser()->getId();
 
         if (!ForumPermissionModel::getInstance()->hasForumPermission($userId, $permCode)) {
-            Flash::send(Alert::ERROR, "Forum", "Vous n'avez pas la permission de consulter ceci");
+            Flash::send(Alert::ERROR, "Forum", "Vous n'avez pas la permission de faire ceci");
             Redirect::redirectPreviousRoute();
         }
     }
 
+    /**
+     * @param string $permCode
+     * @return void
+     * @desc used in controller : Just alert the user if doesn't have permission, action required in addition to this, to properly process the request
+     */
     public function alertNotHavePermissions(string $permCode): void
     {
         if ($permCode === "user_create_topic_tag") {
             Flash::send(Alert::ERROR, "Forum", "Vous ne pouvez pas définir de tag, ce champ à été vidé");
+        }
+        if ($permCode === "user_response_topic") {
+            Flash::send(Alert::ERROR, "Forum", "Vous n'avez pas la permission de répondre au topic");
         }
     }
 
