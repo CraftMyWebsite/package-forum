3e34916bf805ca266a5265c3fea780b926d7a302
Léo <69589034+Zomblard@users.noreply.github.com>
2023-09-24 11:55:11 +0200

    [ADD] default perm by role

--- Init/init.sql
+++ Init/init.sql
@@ -256,10 +256,8 @@ VALUES (1, NULL, 'operator'),
        (23, NULL, 'admin_set_closed'),
        (24, NULL, 'admin_move_topic');
 
-INSERT INTO `cmw_forums_roles_permissions` (`forum_permission_id`, `forum_role_id`)
-VALUES ('1', '3');
 
-
+INSERT INTO `cmw_forums_roles_permissions` (`forum_permission_id`, `forum_role_id`) VALUES ('1', '3'),('2', '2'), ('3', '2'), ('4', '2'), ('5', '2'), ('6', '2'), ('7', '2'), ('8', '2'), ('9', '2'), ('10', '2'), ('11', '2'), ('12', '2'), ('13', '2'), ('14', '2'), ('15', '2'), ('16', '2'), ('17', '2'), ('21', '2'), ('22', '2'), ('23', '2'),('2', '1'), ('3', '1'), ('4', '1'), ('5', '1'), ('11', '1'), ('12', '1'), ('13', '1'), ('14', '1'), ('15', '1'), ('16', '1'), ('17', '1');
 
 
 CREATE TABLE IF NOT EXISTS cmw_forums_discord
